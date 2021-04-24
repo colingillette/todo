41ad12c13917f889ddf08c040b61ca02e9e5b20a
Colin Gillette <colingillette35@gmail.com>
2021-04-24 17:29:42 -0500

    tweak wording and apostrophe style

--- index.js
+++ index.js
@@ -14,34 +14,34 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var task = ["buy socks", "practise with nodejs"];
-var complete = ["finish jQuery"];
+var task = ['buy socks', 'practise with nodejs'];
+var complete = ['finish jQuery'];
 
 app.post('/addtask', function (req, res) {
     var newTask = req.body.newtask;
     task.push(newTask);
-    res.redirect("/");
+    res.redirect('/');
 });
 
 app.post('/removetask', function(req, res) {
     var completeTask = req.body.check;
-    if (typeof completeTask === "string") {
+    if (typeof completeTask === 'string') {
         complete.push(completeTask);
         task.splice(task.indexOf(completeTask), 1);
-    } else if (typeof completeTask === "object") {
+    } else if (typeof completeTask === 'object') {
         for (var i = 0; i < completeTask.length; i++) {     
             complete.push(completeTask[i]);
             task.splice(task.indexOf(completeTask[i]), 1);
         }
     }
-    res.redirect("/");
+    res.redirect('/');
 });
 
-app.get("/", function(req, res) {    
-  res.render("index", { task: task, complete: complete });
+app.get('/', function(req, res) {    
+  res.render('index', { task: task, complete: complete });
 });
 
 app.listen(3000, function () {
